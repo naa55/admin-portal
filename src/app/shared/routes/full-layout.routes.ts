f5e5c6168c9bca50514394dad119fcd5c5dba146
Naa55 <bettyablorh777@gmail.com>
2024-09-05 19:12:40 +0000

    first commit

--- src/app/shared/routes/full-layout.routes.ts
+++ src/app/shared/routes/full-layout.routes.ts
@@ -9,11 +9,7 @@ export const Full_ROUTES: Routes = [
          canActivate: [AuthGuard],
         loadChildren: () => import('../../dashboard/dashboard.module').then(m => m.DashboardModule)
     },
-    {
-        path: 'application',
-        loadChildren: () => import('../../application/application.module').then(m => m.ApplicationModule)
-
-    },
+    
     {
         path: 'user-management',
         canActivate: [AuthGuard],
@@ -23,12 +19,6 @@ export const Full_ROUTES: Routes = [
     {
         path: 'case-law',
         loadChildren: () => import('../../case-law/case-law.module').then(m => m.CaseLawModule),
-        canActivate: [AuthGuard]
-
-    },
-    {
-        path: 'contact',
-        loadChildren: () => import('../../contact/contact.module').then(m => m.ContactModule),
         canActivate: [AuthGuard]
 
     },
@@ -75,16 +65,6 @@ export const Full_ROUTES: Routes = [
 
     },
     {
-        path: 'widgets',
-        loadChildren: () => import('../../widgets/widgets.module').then(m => m.WidgetsModule)
-
-    },
-    {
-        path: 'ecommerce',
-        loadChildren: () => import('../../ecommerce/ecommerce.module').then(m => m.EcommerceModule)
-
-    },
-    {
         path: 'admin-users',
         loadChildren: () => import('../../admin-users/admin-users.module').then(m => m.AdminUsersModule),
         canActivate: [AuthGuard]
@@ -95,56 +75,15 @@ export const Full_ROUTES: Routes = [
         loadChildren: () => import('../../components/components.module').then(m => m.ComponentsModule)
     },
     {
-        path: 'content',
-        loadChildren: () => import('../../content/content.module').then(m => m.ContentModule)
-    },
-    {
-        path: 'icons',
-        loadChildren: () => import('../../icons/icons.module').then(m => m.IconsModule)
-    },
-    {
         path: 'form',
         loadChildren: () => import('../../form/form.module').then(m => m.FormModule)
-    },
-    {
-        path: 'table',
-        loadChildren: () => import('../../table/table.module').then(m => m.TableModule)
-
     },
     {
         path: 'user-profile',
         loadChildren: () => import('../../user-profile/user-profile.module').then(m => m.UserProfileModule)
 
     },
-    {
-        path: 'faq',
-        loadChildren: () => import('../../faq/faq.module').then(m => m.FaqModule)
-    },
-    {
-        path: 'pricing',
-        loadChildren: () => import('../../pricing/pricing.module').then(m => m.PricingModule)
-    },
-    {
-        path: 'earnings',
-        loadChildren: () => import('../../earnings/earnings.module').then(m => m.EarningsModule)
-    },
-    {
-        path: 'downloads',
-        loadChildren: () => import('../../downloads/downloads.module').then(m => m.DownloadsModule)
-    },
-    {
-        path: 'timeline',
-        loadChildren: () => import('../../timeline/timeline.module').then(m => m.TimelineModule)
-    },
-    {
-        path: 'charts',
-        loadChildren: () => import('../../charts/chart.module').then(m => m.ChartModule)
-    },
-    {
-        path: 'maps',
-        loadChildren: () => import('../../maps/maps.module').then(m => m.MapsModule)
-
-    },{
+   {
         path: 'guest',
         loadChildren: () => import('../../guest/guest.module').then(m => m.GuestModule)
     },
